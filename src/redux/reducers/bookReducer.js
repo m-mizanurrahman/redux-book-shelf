b08bce099728244817a4fb847784e0fce27db4d9
mrahman96 <mizan38ju@gmail.com>
2021-05-25 21:46:10 -0600

    redux-fetching

--- src/redux/reducers/bookReducer.js
+++ src/redux/reducers/bookReducer.js
@@ -1,23 +1,29 @@
+import allBooks from '../../fakeData/books.json'
 const initialState = {
     readingList: [],
-    discoverList: [],
+    discoverList: allBooks,
     finishedList: []
 }
 
-const bookReducer = (state, action) => {
+const bookReducer = (state = initialState, action) => {
 
     switch (action.type) {
         case 'ADD_TO_READING_LIST': {
+            return state
 
         }
 
         case 'REMOVE_FROM_READING_LIST': {
+            return state
 
         }
 
         default: {
+            return state
 
         }
     }
 
 };
+
+export default bookReducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,10 @@
 import { createStore, combineReducers } from "redux"
+import { composeWithDevTools } from "redux-devtools-extension"
 import bookReducer from "./reducers/bookReducer"
 
 const combinedReducer = combineReducers({
     books: bookReducer,
     // user: useReducer,
-
-
 })
 
-
-export const store = createStore(combinedReducer)
+export const store = createStore(combinedReducer, composeWithDevTools())
